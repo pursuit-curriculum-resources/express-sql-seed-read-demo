27335f2e7a77c52256060476eaa7cdc02dc1d141
Karolin Rafalski <kr2174@caa.columbia.edu>
2023-08-14 10:23:51 -0400

    index route done

---
+++ app.js
@@ -0,0 +1,26 @@
+// DEPENDENCIES
+const cors = require("cors");
+const express = require("express");
+
+// CONFIGURATION
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to Bookmarks App");
+});
+
+const bookmarksController = require("./controllers/bookmarksController.js");
+app.use("/bookmarks", bookmarksController);
+
+// 404 PAGE
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
+
+// EXPORT
+module.exports = app;
